459783fe3e7378405d920910838e9f0352614cba
乐天居易 <19836788252@163.com>
2024-03-25 16:50:08 +0800

    获取轮播图数据

--- local_life/pages/home/home.js
+++ local_life/pages/home/home.js
@@ -5,16 +5,27 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    // 存放轮播图数据的列表
+    swiperList:[]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-
+    this.getSwiperList()
   },
-
+  // 获取轮播图数据的方法
+  getSwiperList(){
+    wx.request({
+      url: 'https://applet-base-api-t.itheima.net/slides',
+      method:'GET',
+      success:(res)=>{
+        console.log(res.data)
+        this.setData(this.data.swiperList =res.data)
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
