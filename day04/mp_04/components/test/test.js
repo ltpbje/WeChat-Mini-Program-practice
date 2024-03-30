2573ac0788c6c103f76ed85d4ad078c26a6c0c03
乐天居易 <19836788252@163.com>
2024-03-30 10:47:24 +0800

    自定义组件的数据和方法

--- day04/mp_04/components/test/test.js
+++ day04/mp_04/components/test/test.js
@@ -1,6 +1,6 @@
 // components/test/test.js
 Component({
-
+  
   /**
    * 组件的属性列表
    */
@@ -12,13 +12,25 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    count:0
   },
-
   /**
    * 组件的方法列表
    */
   methods: {
-
+    // 事件处理函数
+    addCount(){
+      this.setData({
+        count:this.data.count + 1
+      })
+      this._showCount()
+    },
+    // 自定义函数
+    _showCount(){
+      wx.showToast({
+        title: `count是${this.data.count}`,
+        icon:"none"
+      })
+    }
   }
 })
