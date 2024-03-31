64efb90ed7253bb0a893645cbfe475ee7bb7cf5a
乐天居易 <19836788252@163.com>
2024-03-31 16:53:41 +0800

    生成随机颜色案例

--- day04/mp_04/components/test3/test3.js
+++ day04/mp_04/components/test3/test3.js
@@ -42,6 +42,17 @@ Component({
         '_rgb.b':this.data._rgb.b <255 ?  this.data._rgb.b +5 : 255 
       })
     },
+    // 产生随机颜色
+    _randomColor(){
+      this.setData({
+        _rgb:{
+        r:Math.floor(Math.random() *256),
+        g:Math.floor(Math.random() *256),
+        b:Math.floor(Math.random()*256)
+       }
+      })
+    },
+
    },
    observers:{
     // '_rgb.r,_rgb.g,_rgb.b' : function(r,g,b){
@@ -54,5 +65,33 @@ Component({
         fullColor:`${rgb.r},${rgb.g},${rgb.b}`
       })
     }
+    },
+    // 组件生命周期函数
+    // 组件生命周期声明对象，组件的生命周期：created、attached、ready、moved、detached 将收归到 lifetimes 字段内进行声明，原有声明方式仍旧有效，如同时存在两种声明方式，则 lifetimes 字段内声明方式优先级最高
+    lifetimes:{
+      //在组件实例刚刚被创建时执行，注意此时不能调用 setData
+      created(){
+        // console.log("created~~~~")
+      },
+      attached(){
+        // console.log("attached~~~~")
+      }
+    },
+    // pageLifetimes 组件所在页面的生命周期声明对象
+    pageLifetimes:{
+      // 页面生命周期回调—监听页面显示
+      // 页面显示/切入前台时触发。
+      show: function(){
+        console.log('show')
+        this._randomColor()
+      },
+      // 页面隐藏/切入后台时触发。 如 navigateTo 或底部 tab 切换到其他页面，小程序切入后台等。
+      hide:function(){
+        console.log('hide')
+      },
+      // 所在页面尺寸变化时执行
+      resize(){
+        console.log('resize')
+      }
     }
 })
