6bb128932ffe1481cb0086f0d4c9243df6ddb178
乐天居易 <19836788252@163.com>
2024-04-01 21:01:43 +0800

    实现api的promise化

--- day05/mp_05/app.js
+++ day05/mp_05/app.js
@@ -1,4 +1,10 @@
 //app.js
+// 小程序入口文件中（app.js），只需调用一次promisifyAll（）方法，
+// 即可实现异步API的Promise化
+import {promisifyAll} from 'miniprogram-api-promise'
+const wxp =wx.p ={}
+// promisify all wx's api
+promisifyAll(wx,wxp)
 App({
   onLaunch: function () {
     // 展示本地存储能力
