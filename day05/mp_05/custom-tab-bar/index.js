5c7ae2464d69781a79510eec474ef0e629a8655b
乐天居易 <19836788252@163.com>
2024-04-04 21:17:30 +0800

    初步实现tab页切换

--- day05/mp_05/custom-tab-bar/index.js
+++ day05/mp_05/custom-tab-bar/index.js
@@ -33,20 +33,20 @@ Component({
     active: 0,
     list: [
       {
-        "pagePath": "pages/home/home",
+        "pagePath": "/pages/home/home",
         "text": "首页",
         "iconPath": "/images/tabs/home.png",
         "selectedIconPath": "/images/tabs/home-active.png"
       },
       {
-        "pagePath": "pages/message/message",
+        "pagePath": "/pages/message/message",
         "text": "消息",
         "iconPath": "/images/tabs/message.png",
         "selectedIconPath": "/images/tabs/message-active.png",
         info:2
       },
       {
-        "pagePath": "pages/contact/contact",
+        "pagePath": "/pages/contact/contact",
         "text": "联系我们",
         "iconPath": "/images/tabs/contact.png",
         "selectedIconPath": "/images/tabs/contact-active.png"
@@ -59,7 +59,10 @@ Component({
    */
   methods: {
     onChange(event) {
-      this.setData({ active: event.detail });
+      this.setData({ active: event.detail })
+      wx.switchTab({
+        url: this.data.list[event.detail].pagePath,
+      })
     },
   }
 })
