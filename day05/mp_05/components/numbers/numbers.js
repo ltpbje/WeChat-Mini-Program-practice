4aab6ec063c1a3d518cda22b78644ffab4673e69
乐天居易 <19836788252@163.com>
2024-04-04 09:52:08 +0800

    在组件中使用Store中的成员

--- day05/mp_05/components/numbers/numbers.js
+++ day05/mp_05/components/numbers/numbers.js
@@ -1,9 +1,23 @@
 // components/numbers/numbers.js
+import {storeBindingsBehavior} from 'mobx-miniprogram-bindings'
+import {store} from '../../store/store'
 Component({
-
+  behaviors:[storeBindingsBehavior], //通过storeBindingsBehavior来实现自动绑定
   /**
    * 组件的属性列表
    */
+  storeBindings:{
+    store,//指定要绑定的Store
+    fields:{
+      numA:()=>store.numA, //绑定字段的第1种方式
+      numB:(store)=>store.numB,//绑定字段的第2种方式
+      sum:'sum' //绑定字段的第3种方式
+    },
+    actions:{//指定要绑定的方法
+      updateNum2:'updateNum2'
+    }
+  },
+
   properties: {
 
   },
@@ -19,6 +33,8 @@ Component({
    * 组件的方法列表
    */
   methods: {
-
+    btnHandle2(e){
+      this.updateNum2(e.target.dataset.step)
+    }
   }
 })
